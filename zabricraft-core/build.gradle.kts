3523f5ad2d82302208b7936955728af86aec6128
NathanFallet <contact@nathanfallet.me>
2023-11-20 23:55:25 +0100

    Using api() to expose runtime from core

--- zabricraft-core/build.gradle.kts
+++ zabricraft-core/build.gradle.kts
@@ -26,5 +26,5 @@ kotlin {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
-    compileOnly(project(":zabricraft-runtime"))
+    api(project(":zabricraft-runtime"))
 }
